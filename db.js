0c3961d8ec9b06c7223f5c03197d1811ddf83417
Milena Maltsev <milena@cricketscircle.com>
2015-10-14 17:54:23 -0400

    removed unnecessary things

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://Meli:123fourfive@ds037824.mongolab.com:37824/flights');
+mongoose.connect('mongodb://@ds037824.mongolab.com:37824/flights');
 module.exports = mongoose.connection;
